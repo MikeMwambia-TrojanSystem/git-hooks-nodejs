9b1ff3ea64617a086b600cd2afe6065b66d84958
Mzynga.eth <mikemwambia@gmail.com>
2021-12-17 10:24:44 +0300

    pre-commit with husky

--- test/mult.test.js
+++ test/mult.test.js
@@ -1,6 +1,6 @@
 const mult = require('../src/mutiply');
 
 it('should calculate mutliplication of two number',()=>{
-    const result = mult(3,4);
-    expect(result).toBe(12);
+    const result = mult(5,4);
+    expect(result).toBe(20);
 })
